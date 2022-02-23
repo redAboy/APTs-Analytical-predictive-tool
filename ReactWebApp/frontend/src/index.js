270e59c5da2f4ac960c1efdd577628edd888de2d
Emotet_Hunter <69321294+Emotet-hunter@users.noreply.github.com>
2022-02-23 12:51:58 +0100

    Add files via upload

---
+++ ReactWebApp/frontend/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import './assets/css/grid.css'
+import './assets/css/index.css'
+
+import Layout from './components/layout/Layout'
+
+document.title = 'APTs Analyzer'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
